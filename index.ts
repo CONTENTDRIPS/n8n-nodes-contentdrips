2883ba2776b8c64a0cdd54cdc7585f18c14a663c
usama <ukhalid639@gmail.com>
2025-07-01 19:45:18 +0500

    fx

---
+++ index.ts
@@ -0,0 +1,5 @@
+// This file serves as the entry point for the n8n custom node package
+// It doesn't need to export anything - n8n will discover the nodes and credentials
+// through the package.json n8n configuration section
+
+export {}; 
